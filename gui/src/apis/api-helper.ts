ccc52e3d0dae0a800e1f3add4d033bf1ef99478f
jacob6838 <jfrye@neaeraconsulting.com>
2023-02-14 15:35:14 -0700

    updating authApiHelper, creating template map notification page

--- gui/src/apis/api-helper.ts
+++ gui/src/apis/api-helper.ts
@@ -1,17 +1,11 @@
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
-const MESSAGE_MONITOR_ENDPOINT = "http://localhost:8081" //process.env.MESSAGE_MONITOR_API_ENDPOINT;
+const MESSAGE_MONITOR_ENDPOINT = "http://localhost:8081"; //process.env.MESSAGE_MONITOR_API_ENDPOINT;
 
 class AuthApiHelper {
-  formatQueryParams(query_params: { [key: string]: string }) {
-    if (!query_params) return;
-    const params: string[] = [];
-    for (const key in query_params) {
-      if (query_params[key] !== "" && query_params[key] !== null) {
-        params.push(`${key}=${query_params[key].replace(" ", "%20")}`);
-      }
-    }
-    return !query_params ? "" : "?" + params.join("&");
+  formatQueryParams(query_params: Record<string, any>): string {
+    if (!query_params) return "";
+    return `?${new URLSearchParams().toString()}`;
   }
 
   async invokeApi({
@@ -26,42 +20,46 @@ class AuthApiHelper {
     successMessage = "Successfully completed request!",
     failureMessage = "Request failed to complete",
   }: {
-    path: string,
-    method?: string,
-    headers?: { [key: string]: string },
-    queryParams?: { [key: string]: string },
-    body?: Object,
-    token: string,
-    toastOnFailure?: boolean,
-    toastOnSuccess?: boolean,
-    successMessage?: string,
-    failureMessage?: string,
-  }) {
-    const url = MESSAGE_MONITOR_ENDPOINT! + path + this.formatQueryParams(queryParams)
-        console.log("MAKING REQUEST TO", url)
-        const results = method === "GET" ? await fetch(url, {
-            method,
-            headers,
-          }) : await fetch(url, {
-            method,
-            headers,
-            body: JSON.stringify(body),
-          });
+    path: string;
+    method?: string;
+    headers?: Record<string, string>;
+    queryParams?: Record<string, string>;
+    body?: Object;
+    token: string;
+    toastOnFailure?: boolean;
+    toastOnSuccess?: boolean;
+    successMessage?: string;
+    failureMessage?: string;
+  }): Promise<any> {
+    const url = MESSAGE_MONITOR_ENDPOINT! + path + this.formatQueryParams(queryParams);
+    console.debug("MAKING REQUEST TO", url);
 
-          const msg = await results.json();
-          if (results.status !== 200 && results.status !== 202) {
-            if (toastOnFailure) toast.error(failureMessage);
-            console.error("Request failed with status code " + results.status + ": " + msg);
-          } else if (msg?.code >= 400 && msg?.code < 500) {
-            if (toastOnFailure) toast.error(failureMessage);
-            console.error("Request failed with status code " + msg.code + ": " + msg.message);
-          } else {
-            if (toastOnSuccess) {
-                toast.success(successMessage);
-            }
-          }
+    const localHeaders: HeadersInit = { ...headers };
+    if (method === "POST" && body) {
+      localHeaders["Content-Type"] = "application/json";
+    }
 
-          return msg;
+    const options: RequestInit = {
+      method: method,
+      headers: localHeaders,
+      body: body ? JSON.stringify(body) : null,
+    };
+
+    return await fetch(url, options)
+      .then((response: Response) => {
+        if (response.ok) {
+          if (toastOnSuccess) toast.success(successMessage);
+        } else {
+          throw new Error(
+            "Request failed with status code " + response.status + ": " + response.statusText
+          );
+        }
+        return response.json();
+      })
+      .catch((error: Error) => {
+        if (toastOnFailure) toast.error(failureMessage);
+        console.error(error.message);
+      });
   }
 }
 
